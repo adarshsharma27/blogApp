a64a284625b32b51eea0573dad2ca908cdcea604
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2023-10-18 21:26:18 +0530

    Adding blog to Appwrite Integration done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-import { Client, Account } from "appwrite";
+import { Client, Account,Databases } from "appwrite";
 
 export const client = new Client();
 client
@@ -6,4 +6,5 @@ client
   .setProject("6522d5121baf4b3c7fad"); // Replace with your project ID
 
 export const account = new Account(client);
+export const databases = new Databases(client);
 export { ID } from "appwrite";
