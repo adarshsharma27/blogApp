fd7aee3482db40ade534b0ea0bc54fd71538f7aa
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2023-12-10 18:44:53 +0530

    Footer Text Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -82,14 +82,14 @@ const Footer = () => {
         <div className="bg-gray-100 dark:bg-slate-700">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p className="text-gray-500 text-sm text-center sm:text-left">
-              © 2020 Tailblocks —
+              ©2023 DesiBlogs —
               <a
                 href="https://twitter.com/knyttneve"
                 rel="noopener noreferrer"
                 className="text-gray-600 ml-1"
                 target="_blank"
               >
-                @knyttneve
+                @AdarshSharma
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
