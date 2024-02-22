d82fdacf8714659eef8ed60b8a871bf0ca0b82c3
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-02-23 00:46:13 +0530

    fix: Duplicate Bookmarks Blogs fixed

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,44 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
-import { LuBookmarkPlus, LuBookmarkMinus, LuTrash2,LuPencilLine  } from "react-icons/lu";
+import {
+  LuBookmarkPlus,
+  LuBookmarkMinus,
+  LuTrash2,
+  LuPencilLine,
+} from "react-icons/lu";
 import { databases, ID } from "../config";
 import conf from "../conf/conf";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { NotificationAudio } from "../utils/NotificationAudio";
 
-const BlogCard = ({ title, category, description, $id, imageUrl,user_Id ,marginTrendingSlider}) => {
+const BlogCard = ({
+  title,
+  category,
+  description,
+  $id,
+  imageUrl,
+  user_Id,
+  marginTrendingSlider,
+}) => {
   const pageUrl = useLocation();
   const [bookmark, setBookMark] = useState();
-  const {userId} = useSelector((state) => state.persistedReducer?.userData);
+  const { userId } = useSelector((state) => state.persistedReducer?.userData);
   useEffect(() => {
     if (pageUrl?.pathname === "/bookmark") setBookMark(true);
     else setBookMark(false);
   }, []);
-
   const navigate = useNavigate();
   const addBookMark = async () => {
-    databases.createDocument(
-      conf.databaseId,
-      conf.bookMarkCollectionId,
-      ID.unique(),
-      {
-        title,
-        category,
-        description,
-        imageUrl: imageUrl ? imageUrl : "https://dummyimage.com/1203x503",
-      }
-    );
-    toast.success("Bookmark Saved", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-    navigate("/bookmark");
+    try {
+      const response = await databases.createDocument(
+        conf.databaseId,
+        conf.bookMarkCollectionId,
+        $id,
+        {
+          title,
+          category,
+          description,
+          imageUrl: imageUrl ? imageUrl : "https://dummyimage.com/1203x503",
+        }
+      );
+      toast.success("Bookmark Saved", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      navigate("/bookmark");
+    } catch (error) {
+      toast.error("Bookmark Already Saved", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   };
   const removeBookMark = async () => {
     try {
@@ -71,9 +95,10 @@ const BlogCard = ({ title, category, description, $id, imageUrl,user_Id ,marginT
   };
   const deleteBlog = async () => {
     try {
+      await databases.deleteDocument(conf.databaseId, conf.collectionId, $id);
       await databases.deleteDocument(
         conf.databaseId,
-      conf.collectionId,
+        "65390dca0d53e330d986",
         $id
       );
       toast.success("Blog Removed Successfully", {
@@ -98,11 +123,16 @@ const BlogCard = ({ title, category, description, $id, imageUrl,user_Id ,marginT
         progress: undefined,
       });
     }
-  
   };
   return (
     <>
-      <div className={marginTrendingSlider ? `m-2 p-4 card-shadow-custom break-words rounded-lg`:`p-4 sm:mb-0 mb-6 card-shadow-custom break-words rounded-lg`}>
+      <div
+        className={
+          marginTrendingSlider
+            ? `m-2 p-4 card-shadow-custom break-words rounded-lg`
+            : `p-4 sm:mb-0 mb-6 card-shadow-custom break-words rounded-lg`
+        }
+      >
         <div className="rounded-lg h-64 overflow-hidden">
           <img
             alt="content"
@@ -113,8 +143,12 @@ const BlogCard = ({ title, category, description, $id, imageUrl,user_Id ,marginT
         <h2 className="text-xl font-bold font-montserrat text-gray-900 mt-5 dark:text-white capitalize">
           {title}
         </h2>
-        <p className="text-lg font-semibold leading-relaxed mt-2 dark:text-gray-300 capitalize">{category}</p>
-        <p className="text-base leading-relaxed mt-2 dark:text-gray-400 capitalize">{description?.slice(0,200)}...</p>
+        <p className="text-lg font-semibold leading-relaxed mt-2 dark:text-gray-300 capitalize">
+          {category}
+        </p>
+        <p className="text-base leading-relaxed mt-2 dark:text-gray-400 capitalize">
+          {description?.slice(0, 200)}...
+        </p>
         <NavLink
           to={`/addblog/${$id}`}
           className="text-purple-500 inline-flex items-center py-3  font-semibold underline underline-offset-2 "
@@ -132,38 +166,34 @@ const BlogCard = ({ title, category, description, $id, imageUrl,user_Id ,marginT
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </NavLink>
-        
-        <div className="flex flex-wrap gap-3 justify-end">        
-          {
-            userId==user_Id &&
-          <LuPencilLine 
+
+        <div className="flex flex-wrap gap-3 justify-end">
+          {userId == user_Id && (
+            <LuPencilLine
               className="text-3xl hover:text-indigo-400 hover:cursor-pointer dark:text-white"
               onClick={() => navigate(`/updateblog/${$id}`)}
             />
-          }
-          {
-            userId==user_Id &&
-          <LuTrash2
+          )}
+          {userId == user_Id && (
+            <LuTrash2
               className="text-3xl hover:text-red-400 hover:cursor-pointer dark:text-white"
               onClick={() => deleteBlog()}
             />
-          }
-          {bookmark  ? (
+          )}
+          {bookmark ? (
             <LuBookmarkMinus
               className="text-3xl hover:text-green-600 hover:cursor-pointer dark:text-white"
               onClick={() => removeBookMark()}
             />
           ) : (
-            userId==user_Id && <LuBookmarkPlus
-              className="text-3xl hover:text-green-600 hover:cursor-pointer dark:text-white"
-              onClick={() => addBookMark()}
-            />
+            userId == user_Id && (
+              <LuBookmarkPlus
+                className="text-3xl hover:text-green-600 hover:cursor-pointer dark:text-white"
+                onClick={() => addBookMark()}
+              />
+            )
           )}
         </div>
-        
-         
-        
-       
       </div>
     </>
   );
