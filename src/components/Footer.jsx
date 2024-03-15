c1bdd9a78bd7e36c0aa2b944125fa744de4868c3
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-03-15 23:34:25 +0530

    fix: Active Link styling fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const { t } = useTranslation();
@@ -18,12 +18,12 @@ const Footer = () => {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                  <NavLink
+                  <Link
                     className="text-gray-600 hover:text-gray-800 dark:text-white"
                     to="/aboutus"
                   >
                     {t("navigationTitle.About Us")}
-                  </NavLink>
+                  </Link>
                 </li>
               </nav>
             </div>
@@ -33,12 +33,12 @@ const Footer = () => {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                  <NavLink
+                  <Link
                     className="text-gray-600 hover:text-gray-800 dark:text-white"
                     to="/signup"
                   >
                     {t("commonTitle.signUpTitle")}
-                  </NavLink>
+                  </Link>
                 </li>
               </nav>
             </div>
@@ -48,12 +48,12 @@ const Footer = () => {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                  <NavLink
+                  <Link
                     className="text-gray-600 hover:text-gray-800 dark:text-white"
                     to="/login"
                   >
                     {t("commonTitle.logInTitle")}
-                  </NavLink>
+                  </Link>
                 </li>
               </nav>
             </div>
@@ -63,12 +63,12 @@ const Footer = () => {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                  <NavLink
+                  <Link
                     className="text-gray-600 hover:text-gray-800 dark:text-white"
                     to="/addblog"
                   >
                     {t("navigationTitle.AddBlog")}
-                  </NavLink>
+                  </Link>
                 </li>
               </nav>
             </div>
@@ -78,17 +78,17 @@ const Footer = () => {
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p className="text-gray-500 text-sm text-center sm:text-left">
             {t('footer. ©2023 DesiBlogs —')}
-              <NavLink
+              <Link
                 href="https://frontendadarshsharma.netlify.com/"
                 rel="noopener noreferrer"
                 className="text-gray-600 ml-1"
                 target="_blank"
               >
                {t('footer.@AdarshSharma')} 
-              </NavLink>
+              </Link>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <NavLink className="text-gray-500" to="/">
+              <Link className="text-gray-500" to="/">
                 <svg
                   fill="currentColor"
                   strokeLinecap="round"
@@ -99,8 +99,8 @@ const Footer = () => {
                 >
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
-              </NavLink>
-              <NavLink className="ml-3 text-gray-500" to="/">
+              </Link>
+              <Link className="ml-3 text-gray-500" to="/">
                 <svg
                   fill="currentColor"
                   strokeLinecap="round"
@@ -111,8 +111,8 @@ const Footer = () => {
                 >
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
-              </NavLink>
-              <NavLink className="ml-3 text-gray-500" to="/">
+              </Link>
+              <Link className="ml-3 text-gray-500" to="/">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -125,8 +125,8 @@ const Footer = () => {
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
-              </NavLink>
-              <NavLink className="ml-3 text-gray-500" to="/"> 
+              </Link>
+              <Link className="ml-3 text-gray-500" to="/"> 
                 <svg
                   fill="currentColor"
                   stroke="currentColor"
@@ -142,7 +142,7 @@ const Footer = () => {
                   ></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
-              </NavLink>
+              </Link>
             </span>
           </div>
         </div>
